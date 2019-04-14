62c56d9a6de832a2cebdb51dbb49a485d433d64b
arjokast <arjokast@gmail.com>
2019-04-14 18:09:52 +0200

    #master.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,38 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterContentInit(): void {
+    d3.select("#textcontainer")
+      .append("h1")
+      .text("Bro")
+      .style("opacity", 0)
+      .transition()
+      .duration(2000)
+      .style("opacity", 1)
+      .transition()
+      .duration(2000)
+      .style("opacity", 0)
+      .transition()
+      .duration(1)
+      .text("Do you even lift?")
+      .transition()
+      .duration(2000)
+      .style("opacity", 1);
+
+
+      // 
+
+      // .attr("color", "blue")
+     
+      // .attr("color", "red");
+
+      // <h1 id="bro">
+      //   Bro
+      // </h1>
+      
+      // <h1 id="doyoueven">
+      //   Do you even lift?
+      // </h1>
+
+  }
 }
